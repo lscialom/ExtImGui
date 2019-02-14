f8958235b634f9036cb828aefcb7e3d9cdc8a559
Lucas Scialom <lucas.scialom@free.fr>
2019-02-14 16:16:30 +0100

    renamed fps_meter to performances_widget

---
+++ src/performances_widget.cpp
@@ -0,0 +1,32 @@
+#include <ext_imgui.hpp>
+
+#include <imgui.h>
+
+namespace ExtImGui {
+Performances *CreatePerformancesWidget() {
+  auto rawp = new Performances();
+  std::unique_ptr<Performances> p(rawp);
+
+  return static_cast<Performances *>(RegisterObject(std::move(p)));
+};
+
+int Performances::Update() {
+  if (!ImGui::Begin("Performances", &open)) {
+    ImGui::End();
+    return 0;
+  }
+
+  if (ImGui::BeginPopupContextItem()) {
+    if (ImGui::MenuItem("Close"))
+      open = false;
+    ImGui::EndPopup();
+  }
+
+  ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+              1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+  ImGui::End();
+
+  return 0;
+}
+} // namespace ExtImGui
